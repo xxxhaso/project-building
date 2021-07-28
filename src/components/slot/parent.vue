50b3fcd9408a1065004207ababd052d02d1b472d
zp41ef12g1 <xhxhxhy@qq.com>
2021-07-28 11:41:49 +0800

    代码更新

---
+++ src/components/slot/parent.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h3>父组件</h3>
+<son>
+  <h1>子组件插槽标题</h1>
+  <p>子组件内容</p>
+  </son>
+</div>
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+  components: {
+    son
+  },
+  name: 'parent',
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/slot/son.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+ <h1>子组件标题</h1>
+ <slot></slot>
+ <!-- 默认插槽会显示父组件中写在子组件中的数据 -->
+</div>
+</template>
+
+<script>
+export default {
+  name: 'son',
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
